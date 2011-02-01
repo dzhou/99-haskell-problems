bba8330687180291fe6ee8b88ad31643662acddd
Kefei Dan Zhou <kefei.zhou@gmail.com>
2011-02-01 00:25:59 -0500

    simple caesar cipher (not part of 99-problems)

---
+++ Cipher.hs
@@ -0,0 +1,19 @@
+
+
+module Cipher where 
+import Char 
+
+-- Caeser Cipher
+rotate :: Int -> Char -> Char
+rotate n c 
+    | isLower c     = chr (((ord c - 97) + n) `mod` 26 + 97)
+    | isUpper c     = rotate n (toUpper c)
+    | otherwise     = c 
+
+encrypt :: Int -> [Char] -> [Char]
+encrypt n plaintext = map (rotate n) plaintext   
+
+decrypt :: Int -> [Char] -> [Char]
+decrypt n ciphertext = map (rotate (-n)) ciphertext 
+
+
